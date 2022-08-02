e27a625f26b0c750da878e4b5baf46cbd0f0bd81
Christo <ChristoOpperman0@gmail.com>
2022-08-02 12:05:23 +0200

    Adding POJO. ( Closes #2.)

---
+++ src/main/java/model/Pet.java
@@ -0,0 +1,33 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Pet {
+    private PetTye petTye;
+    private Owner owner;
+    private LocalDate birthDate;
+
+    public PetTye getPetTye() {
+        return petTye;
+    }
+
+    public void setPetTye(PetTye petTye) {
+        this.petTye = petTye;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
