e568b2b3bbf8ebaee21eb094b9fa19527b3db1da
Christo <ChristoOpperman0@gmail.com>
2022-08-02 15:27:37 +0200

    Refactoring. (Closes #11)

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/CrudServices.java
@@ -0,0 +1,18 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface CrudServices<T, ID> {
+    public Set<T> findAll();
+
+    public T findById(ID id);
+
+    public T save(T object);
+
+    public void delete(T object);
+
+    public void deleteById(ID id);
+}
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
@@ -2,15 +2,6 @@ package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudServices<Vet, Long>{
 
-public interface VetService {
-
-    public Vet findByLastName(String lastName);
-
-    public Vet findById(Long id);
-
-    public Set<Vet> findAll();
-
-    public Vet save(Vet vet);
 }
