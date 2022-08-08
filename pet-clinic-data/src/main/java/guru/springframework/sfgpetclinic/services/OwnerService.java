3e2b4bf7a911d8ecbb83aeba7c87de9dc6cfbe63
Christo <ChristoOpperman0@gmail.com>
2022-08-08 11:59:56 +0200

    Impl find owner. (Closes #54)

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
@@ -2,7 +2,11 @@ package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudServices<Owner, Long>{
 
     public Owner findByLastName(String lastName);
+
+    public List<Owner> findAllByLastNameLike(String lastName);
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -8,6 +8,9 @@ import guru.springframework.sfgpetclinic.services.PetTypeService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @Profile({"default","map"})
 public class OwnerServiceMap extends AbstractMapService<Owner,Long>
@@ -52,5 +55,13 @@ public class OwnerServiceMap extends AbstractMapService<Owner,Long>
                 .filter(owner -> owner.getLastName().equals(lastName))
                 .findFirst()
                 .orElse(null);
+    }
+
+    @Override
+    public List<Owner> findAllByLastNameLike(String lastName) {
+        return map.values()
+                .stream()
+                .filter(owner -> owner.getLastName().contentEquals(lastName))
+                .collect(Collectors.toList());
     }
 }
