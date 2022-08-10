220f38e96e3ca4cbb471d04d1913396e0bee0124
Christo <ChristoOpperman0@gmail.com>
2022-08-10 10:43:37 +0200

    Add ability to add or update a visit.(Closes #57)

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VisitController.java
@@ -0,0 +1,63 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.model.Visit;
+import guru.springframework.sfgpetclinic.services.PetService;
+import guru.springframework.sfgpetclinic.services.VisitService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.beans.PropertyEditorSupport;
+import java.time.LocalDate;
+
+@Controller
+public class VisitController {
+    private final VisitService visitService;
+    private final PetService petService;
+
+    public VisitController(VisitService visitService, PetService petService) {
+        this.visitService = visitService;
+        this.petService = petService;
+    }
+
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder){
+        dataBinder.setDisallowedFields("id");
+
+        dataBinder.registerCustomEditor(LocalDate.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException{
+                setValue(LocalDate.parse(text));
+            }
+        });
+    }
+
+    @ModelAttribute("visit")
+    public Visit loadPetWithVisit(@PathVariable Long petId, Model model){
+        Pet pet = petService.findById(petId);
+        model.addAttribute("pet",pet);
+        Visit visit = Visit.builder().build();
+        pet.getVisits().add(visit);
+        visit.setPet(pet);
+        return visit;
+    }
+
+    @GetMapping("/owners/{ownerId}/pets/{petId}/visits/new")
+    public String initNewVisitForm(@PathVariable Long petId, Model model){
+        return "pets/createOrUpdateVisitForm";
+    }
+
+    @PostMapping("/owners/{ownerId}/pets/{petId}/visits/new")
+    public String processNewVisitForm(@Valid Visit visit, @PathVariable Long ownerId, BindingResult result){
+        if (result.hasErrors()) {
+            return "pets/createOrUpdateVisitForm";
+        }else{
+            this.visitService.save(visit);
+            return "redirect:/owners/{ownerId}";
+        }
+    }
+}
---
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/VisitControllerTest.java
@@ -0,0 +1,96 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.model.PetType;
+import guru.springframework.sfgpetclinic.services.PetService;
+import guru.springframework.sfgpetclinic.services.VisitService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.util.UriTemplate;
+
+import java.net.URI;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+class VisitControllerTest {
+
+    @Mock
+    PetService petService;
+
+    @Mock
+    VisitService visitService;
+
+    @InjectMocks
+    VisitController visitController;
+
+    private MockMvc mockMvc;
+
+    private final UriTemplate visitsUriTemplate = new UriTemplate("/owners/{ownerId}/pets/{petId}/visits/new");
+    private final Map<String, String> uriVariables = new HashMap<>();
+    private URI visitsUri;
+
+    @BeforeEach
+    void setUp() {
+        Long petId = 1L;
+        Long ownerId = 1L;
+
+        Owner owner = Owner.builder().id(ownerId)
+                                     .lastName("Doe")
+                                     .firstName("Joe")
+                                     .build();
+        PetType petType = PetType.builder().name("Cat").build();
+        Pet pet = Pet.builder().id(petId)
+                               .birthDate(LocalDate.of(2020,01,01))
+                               .name("Cutie")
+                               .visits(new HashSet<>())
+                               .owner(owner)
+                               .petType(petType)
+                               .build();
+
+        when(petService.findById(anyLong())).thenReturn(pet);
+
+        uriVariables.clear();
+        uriVariables.put("ownerId", ownerId.toString());
+        uriVariables.put("petId", petId.toString());
+        visitsUri = visitsUriTemplate.expand(uriVariables);
+
+        mockMvc = MockMvcBuilders
+                    .standaloneSetup(visitController)
+                    .build();
+    }
+
+    @Test
+    void initNewVisitForm() throws Exception{
+        mockMvc.perform(get(visitsUri))
+                .andExpect(status().isOk())
+                .andExpect(view().name("pets/createOrUpdateVisitForm"));
+    }
+
+    @Test
+    void processNewVisitForm() throws Exception {
+        mockMvc.perform(post(visitsUri)
+                            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                            .param("date","2018-11-11")
+                            .param("description","yet another visit"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/owners/{ownerId}"))
+                .andExpect(model().attributeExists("visit"));
+    }
+}
